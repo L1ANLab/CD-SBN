10f339f217be37e7687fb5ff82ba1cca64754063
Watremons <z2331355763@sina.com>
2024-04-28 14:54:41 +0800

    🦄 refactor(induced_graph): rename var

--- graph/induced_graph.cpp
+++ graph/induced_graph.cpp
@@ -8,69 +8,69 @@
 
 InducedGraph::InducedGraph()
 : graph()
-, user_map_{}
-, item_map_{}
-, e_lists_{}
+, user_map{}
+, item_map{}
+, e_lists{}
 {}
 
 InducedGraph::InducedGraph(const Graph& g, uint user1, uint user2, uint item1, uint item2)
 : graph(&g)
-, user_map_{std::min(user1, user2), std::max(user1, user2)}
-, item_map_{std::min(item1, item2), std::max(item1, item2)}
-, e_lists_{{user1, item1}, {user1, item2}, {user2, item1}, {user2, item2}}
+, user_map{std::min(user1, user2), std::max(user1, user2)}
+, item_map{std::min(item1, item2), std::max(item1, item2)}
+, e_lists{{user1, item1}, {user1, item2}, {user2, item1}, {user2, item2}}
 {}
 
 InducedGraph::InducedGraph(InducedGraph& g1, InducedGraph& g2, bool is_union)
 : graph(g1.graph)
-, user_map_(g1.user_map_.size() + g2.user_map_.size())
-, item_map_(g1.item_map_.size() + g2.item_map_.size())
-, e_lists_(g1.e_lists_.size() + g2.e_lists_.size())
+, user_map(g1.user_map.size() + g2.user_map.size())
+, item_map(g1.item_map.size() + g2.item_map.size())
+, e_lists(g1.e_lists.size() + g2.e_lists.size())
 {
     if (is_union)
     {
         std::vector<uint>::iterator it1 = std::set_union(
-            g1.user_map_.begin(), g1.user_map_.end(),
-            g2.user_map_.begin(), g2.user_map_.end(),
-            user_map_.begin()
+            g1.user_map.begin(), g1.user_map.end(),
+            g2.user_map.begin(), g2.user_map.end(),
+            user_map.begin()
         );
-        user_map_.resize(it1 - user_map_.begin());
+        user_map.resize(it1 - user_map.begin());
 
         std::vector<uint>::iterator it2 = std::set_union(
-            g1.item_map_.begin(), g1.item_map_.end(),
-            g2.item_map_.begin(), g2.item_map_.end(),
-            item_map_.begin()
+            g1.item_map.begin(), g1.item_map.end(),
+            g2.item_map.begin(), g2.item_map.end(),
+            item_map.begin()
         );
-        item_map_.resize(it2 - item_map_.begin());
+        item_map.resize(it2 - item_map.begin());
 
         std::vector<std::pair<uint, uint>>::iterator it3 = std::set_union(
-            g1.e_lists_.begin(), g1.e_lists_.end(),
-            g2.e_lists_.begin(), g2.e_lists_.end(),
-            e_lists_.begin()
+            g1.e_lists.begin(), g1.e_lists.end(),
+            g2.e_lists.begin(), g2.e_lists.end(),
+            e_lists.begin()
         );
-        e_lists_.resize(it3 - e_lists_.begin());
+        e_lists.resize(it3 - e_lists.begin());
     }
     else
     {
         std::vector<uint>::iterator it1 = std::set_intersection(
-            g1.user_map_.begin(), g1.user_map_.end(),
-            g2.user_map_.begin(), g2.user_map_.end(),
-            user_map_.begin()
+            g1.user_map.begin(), g1.user_map.end(),
+            g2.user_map.begin(), g2.user_map.end(),
+            user_map.begin()
         );
-        user_map_.resize(it1 - user_map_.begin());
+        user_map.resize(it1 - user_map.begin());
 
         std::vector<uint>::iterator it2 = std::set_intersection(
-            g1.item_map_.begin(), g1.item_map_.end(),
-            g2.item_map_.begin(), g2.item_map_.end(),
-            item_map_.begin()
+            g1.item_map.begin(), g1.item_map.end(),
+            g2.item_map.begin(), g2.item_map.end(),
+            item_map.begin()
         );
-        item_map_.resize(it2 - item_map_.begin());
+        item_map.resize(it2 - item_map.begin());
 
         std::vector<std::pair<uint, uint>>::iterator it3 = std::set_intersection(
-            g1.e_lists_.begin(), g1.e_lists_.end(),
-            g2.e_lists_.begin(), g2.e_lists_.end(),
-            e_lists_.begin()
+            g1.e_lists.begin(), g1.e_lists.end(),
+            g2.e_lists.begin(), g2.e_lists.end(),
+            e_lists.begin()
         );
-        e_lists_.resize(it3 - e_lists_.begin());
+        e_lists.resize(it3 - e_lists.begin());
     }
 }
 
@@ -78,7 +78,7 @@ uint InducedGraph::GetDegree(uint v)
 {
     uint degree = 0u;
 
-    for (auto& e: e_lists_)
+    for (auto& e: e_lists)
         if (e.first == v || e.second == v)
             degree++;
 
--- graph/induced_graph.h
+++ graph/induced_graph.h
@@ -10,18 +10,18 @@ class InducedGraph
 {
 public:
     const Graph* graph;
-    std::vector<uint> user_map_;
-    std::vector<uint> item_map_;
-    std::vector<std::pair<uint, uint>> e_lists_;
+    std::vector<uint> user_map;
+    std::vector<uint> item_map;
+    std::vector<std::pair<uint, uint>> e_lists;
 
 public:
     InducedGraph();
     InducedGraph(const Graph& g, uint user1, uint user2, uint item1, uint item2);
     InducedGraph(InducedGraph& g1, InducedGraph& g2, bool is_union);
 
-    uint NumUsers() const { return user_map_.size(); }
-    uint NumItems() const { return item_map_.size(); }
-    uint NumEdges() const { return e_lists_.size(); }
+    uint NumUsers() const { return user_map.size(); }
+    uint NumItems() const { return item_map.size(); }
+    uint NumEdges() const { return e_lists.size(); }
 
     uint GetDegree(uint v);
 };
