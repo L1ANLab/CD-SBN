624412d999aa889526f04d1243dc9d7a8157ce96
Watremons <z2331355763@sina.com>
2024-05-30 10:44:26 +0800

    ✨ feat(utils): add statistic

---
+++ utils/statistic.cpp
@@ -0,0 +1,77 @@
+#include <vector>
+#include <algorithm>
+#include <iostream>
+#include <cmath>
+#include <filesystem>
+
+#include "utils/types.h"
+#include "utils/statistic.h"
+
+namespace fs=std::filesystem;
+
+std::string Statistic::GenerateStatisticResult()
+{
+    fs::path initial_graph_folder = fs::path(initial_graph_path_str).parent_path();
+    
+    fs::path mid_file_path = initial_graph_folder / fs::path("mid_graph.txt");
+
+    std::string distribution = initial_graph_folder.filename().string();
+    std::string file_info = initial_graph_folder.parent_path().filename().string();
+    std::stringstream ss(file_info); // 将input_str转换为stringstream格式
+    std::string all_keyword_num, keywords_per_vertex_num;
+    std::getline(ss, all_keyword_num, '-');
+    std::getline(ss, keywords_per_vertex_num, '-');
+
+    std:: string result = "";
+    result += "STATISTIC RESULT\n";
+    result += "-------------FILE INFO-------------\n";
+    result += ("Initial Graph File:"+ initial_graph_path_str + "\n");
+    result += "Mid Data File: " + mid_file_path.string() + "\n";
+    result += "\n";
+    result += "-------------SOLVER INFO-------------\n";
+    // result += "Result: {}\n".format([result[1] for result in self.solver_result]);
+    result += "Total Nodes: " + std::to_string(node_num) + "\n";
+    result += "Total Edges: " + std::to_string(edge_num) + "\n";
+    result += "All Keywords: " + all_keyword_num + "\n";
+    result += "Keywords Per Vertex: "+ keywords_per_vertex_num +"\n";
+    result += "Distribution: " + distribution + "\n";
+    result += "\n";
+    result += "-------------QUERY INFO-------------\n";
+    std::string query_keywords_str = "";
+    for (uint keyword: query_keywords)
+    {
+        query_keywords_str += (std::to_string(keyword) + " ");
+    }
+    result += "Query Keywords: " + query_keywords_str + "\n";
+    result += "Query Support Threshold: " + std::to_string(query_support_threshold) + "\n";
+    result += "Query Radius: " + std::to_string(query_radius) + "\n";
+    result += "Query Score Threshold: " + std::to_string(query_score_threshold) + "\n";
+    result += "\n";
+    result += "-------------Pruning INFO-------------\n";
+    result += "Pruning Vertices: " + std::to_string(vertex_pruning_counter) + "\n";
+    result += "Pruning Entries: " + std::to_string(entry_pruning_counter) + "\n";
+    result += "Leaf Nodes: " + std::to_string(leaf_node_counter) + "\n";
+    result += "Pruning Leaf Nodes: " + std::to_string(leaf_node_counter - leaf_node_visit_counter) + "\n";
+    result += "\n";
+    result += "-------------TIME INFO-------------\n";
+    // result += "Started at: " + std::to_string(start_timestamp) + " \tFinished at: " + std::to_string(finish_timestamp) + "\n";
+    float total_time = Duration(start_timestamp) - Duration(finish_timestamp);
+    result += "Total time: " + std::to_string(total_time) + "\n";
+    result += "Obtainment time: " + std::to_string(obtainment_time) + "\n";
+    result += "Refinement time: " + std::to_string(refinement_time) + "\n";
+    result += "-------------OBTAIN TIME INFO-------------\n";
+    result += "Select Greatest Entry in Heap time: " + std::to_string(select_greatest_entry_in_H_time) + "\n";
+    result += "Leaf Node Traverse time: " + std::to_string(leaf_node_traverse_time) + "\n";
+    result += "NonLeaf Node Traverse time: " + std::to_string(nonleaf_node_traverse_time) + "\n";
+    result += "Compute R-Hop time: " + std::to_string(compute_r_hop_time) + "\n";
+    result += "Compute K-Truss time: " + std::to_string(compute_k_truss_time) + "\n";
+    result += "Compute Influential Score time: " + std::to_string(compute_influential_score_time) + "\n";
+    result += "Modify Result Set time: " + std::to_string(modify_result_set_time) + "\n";
+    result += "-------------REFINE TIME INFO-------------\n";
+    result += "Select Greatest Increment Entry in Heap time: " + std::to_string(select_greatest_increment_entry_time) + "\n";
+    result += "Refinement Increment Compute Time: " + std::to_string(refinement_increment_compute_time);
+    result += " for " + std::to_string(refinement_increment_compute_counter) + " times\n";
+    result += "Refinement Graph Update Time: " + std::to_string(refinement_graph_update_time) + "\n";
+    result += "Refinement Graph Copy Time: " + std::to_string(refinement_grah_copy_time) + "\n";
+    return result;
+}
---
+++ utils/statistic.h
@@ -0,0 +1,43 @@
+#include <string>
+#include <vector>
+
+class Statistic
+{
+private:
+    std::string initial_graph_path_str;
+    std::string item_label_list_path_str;
+    std::string update_stream_path_str;
+    std::vector<uint> query_keywords;
+    uint query_support_threshold;
+    uint query_radius;
+    uint query_score_threshold;
+
+public:
+    uint node_num;
+    uint edge_num;
+
+    std::chrono::high_resolution_clock::time_point start_timestamp;
+    std::chrono::high_resolution_clock::time_point finish_timestamp;
+    float select_greatest_entry_in_H_time;
+    float leaf_node_traverse_time;
+    float nonleaf_node_traverse_time;
+    float compute_r_hop_time;
+    float compute_k_truss_time;
+    float compute_influential_score_time;
+    float modify_result_set_time;
+    float obtainment_time;
+    float refinement_time;
+
+    float select_greatest_increment_entry_time;
+    float refinement_increment_compute_time;
+    float refinement_increment_compute_counter;
+    float refinement_graph_update_time;
+    float refinement_grah_copy_time;
+
+    float vertex_pruning_counter;
+    float entry_pruning_counter;
+    float leaf_node_counter;
+    float leaf_node_visit_counter;
+
+    std::string GenerateStatisticResult();
+};
