575f587b03ff283e43439a647126458437444f6e
Watremons <z2331355763@sina.com>
2024-07-03 21:00:01 +0800

    ✨ feat(main): add update stream

--- detection/main.cpp
+++ detection/main.cpp
@@ -10,8 +10,8 @@
 #include "utils/statistic.h"
 
 #include "graph/graph.h"
-#include "detection/snapshot.h"
-#include "detection/continuous.h"
+#include "detection/snapshot_handle.h"
+#include "detection/continuous_handle.h"
 #include "detection/synopsis.h"
 
 
@@ -21,7 +21,7 @@ int main(int argc, char *argv[])
 
     bool is_continuous_flag = false;
     std::string initial_graph_path = "", item_label_list_path = "", update_stream_path = "";
-    uint query_timestamp = -1;
+    uint query_timestamp = -1, sliding_window_size = 0;
     std::vector<uint> query_keywords(0);
     uint query_support_threshold = 0, query_radius = 0, query_score_threshold = 0;
 
@@ -30,6 +30,7 @@ int main(int argc, char *argv[])
     app.add_option("-l,--labels", item_label_list_path, "initial data graph path")->required();
     app.add_option("-u,--update", update_stream_path, "update stream path")->required();
     app.add_option("-t,--qtime", query_timestamp, "query timestamp");
+    app.add_option("-w,--window", sliding_window_size, "the size of sliding window")->required();
     app.add_option("-Q,--qkeywords", query_keywords, "query keywords (a comma separated string)")->required();
     app.add_option("-k,--qsupport", query_support_threshold, "query support threshold")->required();
     app.add_option("-r,--qradius", query_radius, "query maximum radius")->required();
@@ -82,18 +83,107 @@ int main(int argc, char *argv[])
 
     // 3. execute query
     statistic->start_timestamp = start;
-    
-    Snapshot* snapshot_query = new Snapshot(
-        query_timestamp,
-        query_keywords,
-        query_support_threshold,
-        query_radius,
-        query_score_threshold,
-        data_graph,
-        syn
-    );
+    std::vector<InducedGraph*> result_list;
+    if (!is_continuous_flag) // 3.1. for snapshot query
+    {
+        // 3.1.1. maintain the graph and synopsis until the query time
+        if (query_timestamp > data_graph->GetGraphTimestamp())
+        {
+            size_t start_idx = 0, end_idx = 0;
+            std::vector<InsertUnit> update_stream = data_graph->GetUpdateStream();
+            while (query_timestamp <= update_stream[end_idx].timestamp)
+            {
+                // add new edge if in query
+                uint addition_flag = data_graph->InsertEdge(
+                    update_stream[end_idx].user_id,
+                    update_stream[end_idx].item_id
+                );
+                syn->UpdateSynopsisAfterInsertion(
+                    update_stream[end_idx].user_id,
+                    update_stream[end_idx].item_id,
+                    addition_flag,
+                    data_graph
+                );
+                // expire old edge if out of window
+                if (end_idx - start_idx + 1 > sliding_window_size)
+                {
+                    uint removal_flag = data_graph->ExpireEdge(
+                        update_stream[end_idx].user_id,
+                        update_stream[end_idx].item_id
+                    );
+                    syn->UpdateSynopsisAfterExpiration(
+                        update_stream[end_idx].user_id,
+                        update_stream[end_idx].item_id,
+                        removal_flag,
+                        data_graph
+                    );
+                    start_idx += 1;
+                }
+                // move to next edge
+                end_idx += 1;
+            }   
+        }
+        // 3.1.2. find the answer for the snapshot query
+        SnapshotHandle* snapshot_query = new SnapshotHandle(
+            query_keywords,
+            query_support_threshold,
+            query_radius,
+            query_score_threshold,
+            data_graph,
+            syn
+        );
 
-    std::vector<InducedGraph*> result_list = snapshot_query->ExecuteSnapshotQuery(statistic);
+        result_list = snapshot_query->ExecuteSnapshotQuery(statistic);
+    }
+    else // 3.2. for continuous query
+    {
+        size_t start_idx = 0, end_idx = 0;
+        std::vector<InsertUnit> update_stream = data_graph->GetUpdateStream();
+        while (start_idx < update_stream.size())
+        {
+            // 3.2.1. maintain the graph and synopsis once
+            // add new edge if in query
+            uint addition_flag = data_graph->InsertEdge(
+                update_stream[end_idx].user_id,
+                update_stream[end_idx].item_id
+            );
+            syn->UpdateSynopsisAfterInsertion(
+                update_stream[end_idx].user_id,
+                update_stream[end_idx].item_id,
+                addition_flag,
+                data_graph
+            );
+            // expire old edge if out of window
+            if (end_idx - start_idx + 1 > sliding_window_size)
+            {
+                uint removal_flag = data_graph->ExpireEdge(
+                    update_stream[end_idx].user_id,
+                    update_stream[end_idx].item_id
+                );
+                syn->UpdateSynopsisAfterExpiration(
+                    update_stream[end_idx].user_id,
+                    update_stream[end_idx].item_id,
+                    removal_flag,
+                    data_graph
+                );
+                start_idx += 1;
+            }
+            // TODO: 3.2.2. find the answer for the continuous query
+            // SnapshotHandle* snapshot_query = new SnapshotHandle(
+            //     query_keywords,
+            //     query_support_threshold,
+            //     query_radius,
+            //     query_score_threshold,
+            //     data_graph,
+            //     syn
+            // );
+
+            // result_list = snapshot_query->ExecuteSnapshotQuery(statistic);
+        
+            // move to next edge
+            end_idx += 1;
+        }
+    }
     statistic->finish_timestamp = Get_Time();
 
     // TODO: print result
