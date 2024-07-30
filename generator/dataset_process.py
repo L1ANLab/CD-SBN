4cc61208f74bfa61bba21b5acaddaf2cedcd0b3a
Watremons <z2331355763@sina.com>
2024-07-30 17:49:04 +0800

    ✨ feat(generator): Added funcs
    Added query keywords generator;
    Added generator to build dataset frorm raw graph to input graph and keywords list;

--- generator/dataset_process.py
+++ generator/dataset_process.py
@@ -6,6 +6,7 @@ from tqdm import tqdm
 
 ALL_KEYWORD_NUM = 20
 KEYWORDS_PER_VERTEX_NUM = 3
+INITIAL_GRAPH_RATIO = 0.9999
 
 
 class EdgeRawData:
@@ -35,11 +36,9 @@ class EdgeRawData:
         return ("{} {} {}").format(self.user, self.item, self.tstamp)
 
 
-if __name__ == "__main__":
+def GenerateDataset(dir_name: str, file_name: str, distribution: str):
     # 0. Param settings
-    dir_name = "BS"
-    file_path = os.path.join(dir_name, "bibsonomy-2ui/out.bibsonomy-2ui")
-    distribution = "uniform"
+    file_path = os.path.join(dir_name, file_name)
     all_keyword_num = ALL_KEYWORD_NUM
     keywords_per_vertex_num = KEYWORDS_PER_VERTEX_NUM
 
@@ -76,14 +75,13 @@ if __name__ == "__main__":
 
     data_list = sorted(data_list)
 
-    # 3. input file generate
-    initial_graph_size = int(len(data_list) * 0.2)
+    # 3. divide inital graph and update stream
+    initial_graph_size = int(len(data_list) * INITIAL_GRAPH_RATIO)
     initial_graph = data_list[:initial_graph_size]
-
     update_stream = data_list[initial_graph_size:]
 
+    # 4. generate distribution sample
     item_label_list = []
-
     if distribution == "uniform":
         keywords_set = range(0, all_keyword_num)
     elif distribution == "zipf":
@@ -112,6 +110,7 @@ if __name__ == "__main__":
         keywords_set = np.random.normal(mean, stddev, size)
     keywords_set = np.clip(keywords_set, 0, all_keyword_num-1).astype(int)
 
+    # 5. generate keywords following distribution
     label_counter = [0 for _ in range(all_keyword_num)]
     for item_id in range(max_item+1):
         keyword_num = np.random.randint(max(keywords_per_vertex_num-1, 1), keywords_per_vertex_num+2)  # the num is key_per ± 1
@@ -123,6 +122,7 @@ if __name__ == "__main__":
             label_counter[keyword] += 1
         item_label_list.append(keywords)
 
+    # 6. write dataset into files
     initial_graph_file = "initial_graph.txt"
     item_label_list_file = "label_list.txt"
     update_stream_file = "update_stream.txt"
@@ -139,3 +139,102 @@ if __name__ == "__main__":
         wf.write("% min:{} max:{}\n".format(min_tstamp, max_tstamp))
         for data in tqdm(update_stream):
             wf.write("{} {} {}\n".format(data.user, data.item, data.tstamp))
+
+
+def GenerateDatasetWithKeywords(dir_name: str, graph_file_name: str, keyword_file_name: str):
+    # 0. Param settings
+    graph_file_path = os.path.join(dir_name, graph_file_name)
+    keyword_file_path = os.path.join(dir_name, keyword_file_name)
+
+    # 1. Graph file read
+    full_file_path = (os.path.join(os.getcwd(), "dataset", graph_file_path))
+    raw_data_list = []
+    with open(full_file_path, "r") as f:
+        raw_data_list = f.readlines()
+
+    # 2. Raw data process
+    max_item = 0
+    min_tstamp = float("inf")
+    max_tstamp = 0
+
+    data_list = []
+    for line in tqdm(raw_data_list):
+        if line.startswith("%"):
+            continue
+        [user, item, weight, tstamp] = line.strip().split(" ")
+        for _ in range(int(weight)):
+            now_data = EdgeRawData(int(user), int(item), int(weight), int(tstamp))
+            data_list.append(now_data)
+        # now_data = EdgeRawData(int(user), int(item), int(weight), int(tstamp))
+        # data_list.append(now_data)
+        if now_data.item > max_item:
+            max_item = now_data.item
+        if now_data.item > max_tstamp:
+            max_tstamp = now_data.tstamp
+        if now_data.tstamp < min_tstamp:
+            min_tstamp = now_data.tstamp
+
+        if now_data.weight > 1:
+            print(now_data)
+
+    data_list = sorted(data_list)
+
+    # 3. divide inital graph and update stream
+    initial_graph_size = int(len(data_list) * INITIAL_GRAPH_RATIO)
+    initial_graph = data_list[:initial_graph_size]
+    update_stream = data_list[initial_graph_size:]
+
+    # 4. read the keyword file
+    full_keyword_file_path = (os.path.join(os.getcwd(), "dataset", keyword_file_path))
+    raw_keyword_data_list = []
+    with open(full_keyword_file_path, "r") as f:
+        raw_keyword_data_list = f.readlines()
+
+    # 5. keyword data process
+    max_keyword = 0
+    keyword_list = dict()
+    all_keyword_list = []
+    for line in tqdm(raw_keyword_data_list):
+        if line.startswith("%"):
+            continue
+        [keyword, item, weight, tstamp] = line.strip().split(" ")
+        keyword = int(keyword)
+        if item not in keyword_list:
+            keyword_list[item] = [keyword]
+        elif keyword not in keyword_list[item]:
+            keyword_list[item].append(keyword)
+        all_keyword_list.append(keyword)
+
+        if keyword > max_keyword:
+            max_keyword = keyword
+    keyword_list = sorted(keyword_list.items(), key=lambda d: int(d[0]), reverse=False)
+
+    # 6. write dataset into files
+    initial_graph_file = "initial_graph.txt"
+    item_label_list_file = "label_list.txt"
+    update_stream_file = "update_stream.txt"
+    keyword_file = "keywords_list.txt"
+    with open(os.path.join(os.getcwd(), "dataset", dir_name, initial_graph_file), "w") as wf:
+        for data in tqdm(initial_graph):
+            wf.write("{} {} {}\n".format(data.user, data.item, data.tstamp))
+
+    with open(os.path.join(os.getcwd(), "dataset", dir_name, item_label_list_file), "w") as wf:
+        wf.write("{}\n".format(max_keyword+1))
+        for (item_id, labels) in tqdm(keyword_list):
+            wf.write("{} {}\n".format(item_id, ",".join([str(label) for label in sorted(labels)])))
+
+    with open(os.path.join(os.getcwd(), "dataset", dir_name, update_stream_file), "w") as wf:
+        wf.write("% min:{} max:{}\n".format(min_tstamp, max_tstamp))
+        for data in tqdm(update_stream):
+            wf.write("{} {} {}\n".format(data.user, data.item, data.tstamp))
+
+    with open(os.path.join(os.getcwd(), "dataset", dir_name, keyword_file), "w") as wf:
+        wf.write("{}\n".format(max_keyword+1))
+        for keyword in tqdm(all_keyword_list):
+            wf.write("{}\n".format(keyword))
+
+
+if __name__ == "__main__":
+    # GenerateDataset("BS", "out.bibsonomy-2ui", "uniform")
+
+    GenerateDatasetWithKeywords("BS", "out.bibsonomy-2ui", "out.bibsonomy-2ti")
---
+++ generator/query_keywords_generator.py
@@ -0,0 +1,54 @@
+import os
+import numpy as np
+from tqdm import tqdm
+# from fitter import fitter
+
+
+def GenerateQueryKeywords(dir_name: str, file_name: str, query_keywords_size: int, query_keywords_num: int):
+    # 0. Param settings
+    keyword_file_path = os.path.join(dir_name, file_name)
+
+    # 1. keyword file read
+    full_file_path = (os.path.join(os.getcwd(), "dataset", keyword_file_path))
+    raw_keyword_data_list = []
+    with open(full_file_path, "r") as f:
+        total_num = f.readline()
+        raw_keyword_data_list = f.readlines()
+    total_num = int(total_num)
+    # 2. process keyword data
+    keywords_list = [i for i in range(total_num)]
+    keywords_count_list = [0 for _ in range(total_num)]
+    for line in tqdm(raw_keyword_data_list):
+        if line.startswith("%"):
+            continue
+        keyword = int(line.strip().split(" ").pop())
+        keywords_count_list[keyword] += 1
+    keywords_list = sorted(keywords_list, reverse=False)
+    keywords_count_list = np.array(keywords_count_list)
+
+    # 3. Generate query keywords
+    query_keywords_list = []
+    for _ in range(query_keywords_num):
+        query_keywords = np.random.choice(
+            keywords_list,
+            size=query_keywords_size,
+            p=keywords_count_list/sum(keywords_count_list),
+            replace=False
+        )
+        query_keywords_list.append(query_keywords.tolist())
+        # print(query_keywords.tolist())
+
+    # 4. save the query keywords
+    query_keyword_file = "query_keywords_list-{}.txt".format(query_keywords_size)
+    with open(os.path.join(os.getcwd(), "dataset", dir_name, query_keyword_file), "w") as wf:
+        wf.write("{} {}\n".format(query_keywords_num, query_keywords_size))
+        for query_keywords in tqdm(query_keywords_list):
+            wf.write("{}\n".format(" ".join(map(str, query_keywords))))
+
+
+if __name__ == "__main__":
+    GenerateQueryKeywords("BS", "keywords_list.txt", 5, 5)
+    # num_list = [0, 1, 2, 3, 4]
+    # p = np.array([1, 1, 2, 2, 3])
+    # sampling = np.random.choice(num_list, 2, p=p/sum(p), replace=False)
+    # print(sampling)
