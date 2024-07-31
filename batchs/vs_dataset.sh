6e633d00f1946eaca9e414d0d48c46efbe04ac7b
Watremons <z2331355763@sina.com>
2024-07-31 21:53:15 +0800

    🧪 test(batchs): Added user case

--- batchs/vs_dataset.sh
+++ batchs/vs_dataset.sh
@@ -5,10 +5,10 @@ build/cdsbn -i dataset/CM/initial_graph.txt -l dataset/CM/label_list.txt -u data
 build/cdsbn -i dataset/CU/initial_graph.txt -l dataset/CU/label_list.txt -u dataset/CU/update_stream.txt -q dataset/CU/query_keywords_list-5.txt -t 0 -w 100 -k 4 -r 2 -s 2
 build/cdsbn -i dataset/DV/initial_graph.txt -l dataset/DV/label_list.txt -u dataset/DV/update_stream.txt -q dataset/DV/query_keywords_list-5.txt -t 0 -w 100 -k 4 -r 2 -s 2
 build/cdsbn -i dataset/ML/initial_graph.txt -l dataset/ML/label_list.txt -u dataset/ML/update_stream.txt -q dataset/ML/query_keywords_list-5.txt -t 0 -w 100 -k 4 -r 2 -s 2
-# build/cdsbn -i dataset/PL/initial_graph.txt -l dataset/PL/label_list.txt -u dataset/PL/update_stream.txt -q dataset/PL/query_keywords_list-5.txt -t 0 -w 100 -k 4 -r 2 -s 2
+build/cdsbn -i dataset/PL/initial_graph.txt -l dataset/PL/label_list.txt -u dataset/PL/update_stream.txt -q dataset/PL/query_keywords_list-5.txt -t 0 -w 100 -k 4 -r 2 -s 2
 build/cdsbn -i dataset/SX/initial_graph.txt -l dataset/SX/label_list.txt -u dataset/SX/update_stream.txt -q dataset/SX/query_keywords_list-5.txt -t 0 -w 100 -k 4 -r 2 -s 2
 build/cdsbn -i dataset/TA/initial_graph.txt -l dataset/TA/label_list.txt -u dataset/TA/update_stream.txt -q dataset/TA/query_keywords_list-5.txt -t 0 -w 100 -k 4 -r 2 -s 2
-# build/cdsbn -i dataset/UD/initial_graph.txt -l dataset/UD/label_list.txt -u dataset/UD/update_stream.txt -q dataset/UD/query_keywords_list-5.txt -t 0 -w 100 -k 4 -r 2 -s 2
+build/cdsbn -i dataset/UD/initial_graph.txt -l dataset/UD/label_list.txt -u dataset/UD/update_stream.txt -q dataset/UD/query_keywords_list-5.txt -t 0 -w 100 -k 4 -r 2 -s 2
 build/cdsbn -i dataset/UF/initial_graph.txt -l dataset/UF/label_list.txt -u dataset/UF/update_stream.txt -q dataset/UF/query_keywords_list-5.txt -t 0 -w 100 -k 4 -r 2 -s 2
 build/cdsbn -i dataset/VU/initial_graph.txt -l dataset/VU/label_list.txt -u dataset/VU/update_stream.txt -q dataset/VU/query_keywords_list-5.txt -t 0 -w 100 -k 4 -r 2 -s 2
 build/cdsbn -i dataset/WU/initial_graph.txt -l dataset/WU/label_list.txt -u dataset/WU/update_stream.txt -q dataset/WU/query_keywords_list-5.txt -t 0 -w 100 -k 4 -r 2 -s 2
