6d26372f9aaa65147ee30de5e834c639f510adfd
Watremons <z2331355763@sina.com>
2024-09-08 09:22:20 +0800

    ✨ feat(batchs): Add finished batchs

---
+++ batchs/vs_W.sh
@@ -0,0 +1,31 @@
+cd /home/yons/projects/CD-SBN/
+# 1 2 3 4 5
+build/cdsbn -i dataset/synthetic-25k-1/dB/dBkL/initial_graph.txt -l dataset/synthetic-25k-1/dB/dBkL/label_list.txt -u dataset/synthetic-25k-1/dB/dBkL/update_stream.txt -q dataset/synthetic-25k-1/dB/dBkL/query_keywords_list-5.txt -t 0 -w 100 -k 4 -r 2 -s 3
+build/cdsbn -i dataset/synthetic-25k-2/dB/dBkL/initial_graph.txt -l dataset/synthetic-25k-2/dB/dBkL/label_list.txt -u dataset/synthetic-25k-2/dB/dBkL/update_stream.txt -q dataset/synthetic-25k-2/dB/dBkL/query_keywords_list-5.txt -t 0 -w 100 -k 4 -r 2 -s 3
+build/cdsbn -i dataset/synthetic-25k-4/dB/dBkL/initial_graph.txt -l dataset/synthetic-25k-4/dB/dBkL/label_list.txt -u dataset/synthetic-25k-4/dB/dBkL/update_stream.txt -q dataset/synthetic-25k-4/dB/dBkL/query_keywords_list-5.txt -t 0 -w 100 -k 4 -r 2 -s 3
+build/cdsbn -i dataset/synthetic-25k-5/dB/dBkL/initial_graph.txt -l dataset/synthetic-25k-5/dB/dBkL/label_list.txt -u dataset/synthetic-25k-5/dB/dBkL/update_stream.txt -q dataset/synthetic-25k-5/dB/dBkL/query_keywords_list-5.txt -t 0 -w 100 -k 4 -r 2 -s 3
+
+build/cdsbn -i dataset/synthetic-25k-1/dP/dPkL/initial_graph.txt -l dataset/synthetic-25k-1/dP/dPkL/label_list.txt -u dataset/synthetic-25k-1/dP/dPkL/update_stream.txt -q dataset/synthetic-25k-1/dP/dPkL/query_keywords_list-5.txt -t 0 -w 100 -k 4 -r 2 -s 3
+build/cdsbn -i dataset/synthetic-25k-2/dP/dPkL/initial_graph.txt -l dataset/synthetic-25k-2/dP/dPkL/label_list.txt -u dataset/synthetic-25k-2/dP/dPkL/update_stream.txt -q dataset/synthetic-25k-2/dP/dPkL/query_keywords_list-5.txt -t 0 -w 100 -k 4 -r 2 -s 3
+build/cdsbn -i dataset/synthetic-25k-4/dP/dPkL/initial_graph.txt -l dataset/synthetic-25k-4/dP/dPkL/label_list.txt -u dataset/synthetic-25k-4/dP/dPkL/update_stream.txt -q dataset/synthetic-25k-4/dP/dPkL/query_keywords_list-5.txt -t 0 -w 100 -k 4 -r 2 -s 3
+build/cdsbn -i dataset/synthetic-25k-5/dP/dPkL/initial_graph.txt -l dataset/synthetic-25k-5/dP/dPkL/label_list.txt -u dataset/synthetic-25k-5/dP/dPkL/update_stream.txt -q dataset/synthetic-25k-5/dP/dPkL/query_keywords_list-5.txt -t 0 -w 100 -k 4 -r 2 -s 3
+
+build/cdsbn -i dataset/synthetic-25k-1/dB/dBkP/initial_graph.txt -l dataset/synthetic-25k-1/dB/dBkP/label_list.txt -u dataset/synthetic-25k-1/dB/dBkP/update_stream.txt -q dataset/synthetic-25k-1/dB/dBkP/query_keywords_list-5.txt -t 0 -w 100 -k 4 -r 2 -s 3
+build/cdsbn -i dataset/synthetic-25k-2/dB/dBkP/initial_graph.txt -l dataset/synthetic-25k-2/dB/dBkP/label_list.txt -u dataset/synthetic-25k-2/dB/dBkP/update_stream.txt -q dataset/synthetic-25k-2/dB/dBkP/query_keywords_list-5.txt -t 0 -w 100 -k 4 -r 2 -s 3
+build/cdsbn -i dataset/synthetic-25k-4/dB/dBkP/initial_graph.txt -l dataset/synthetic-25k-4/dB/dBkP/label_list.txt -u dataset/synthetic-25k-4/dB/dBkP/update_stream.txt -q dataset/synthetic-25k-4/dB/dBkP/query_keywords_list-5.txt -t 0 -w 100 -k 4 -r 2 -s 3
+build/cdsbn -i dataset/synthetic-25k-5/dB/dBkP/initial_graph.txt -l dataset/synthetic-25k-5/dB/dBkP/label_list.txt -u dataset/synthetic-25k-5/dB/dBkP/update_stream.txt -q dataset/synthetic-25k-5/dB/dBkP/query_keywords_list-5.txt -t 0 -w 100 -k 4 -r 2 -s 3
+
+build/cdsbn -i dataset/synthetic-25k-1/dP/dPkP/initial_graph.txt -l dataset/synthetic-25k-1/dP/dPkP/label_list.txt -u dataset/synthetic-25k-1/dP/dPkP/update_stream.txt -q dataset/synthetic-25k-1/dP/dPkP/query_keywords_list-5.txt -t 0 -w 100 -k 4 -r 2 -s 3
+build/cdsbn -i dataset/synthetic-25k-2/dP/dPkP/initial_graph.txt -l dataset/synthetic-25k-2/dP/dPkP/label_list.txt -u dataset/synthetic-25k-2/dP/dPkP/update_stream.txt -q dataset/synthetic-25k-2/dP/dPkP/query_keywords_list-5.txt -t 0 -w 100 -k 4 -r 2 -s 3
+build/cdsbn -i dataset/synthetic-25k-4/dP/dPkP/initial_graph.txt -l dataset/synthetic-25k-4/dP/dPkP/label_list.txt -u dataset/synthetic-25k-4/dP/dPkP/update_stream.txt -q dataset/synthetic-25k-4/dP/dPkP/query_keywords_list-5.txt -t 0 -w 100 -k 4 -r 2 -s 3
+build/cdsbn -i dataset/synthetic-25k-5/dP/dPkP/initial_graph.txt -l dataset/synthetic-25k-5/dP/dPkP/label_list.txt -u dataset/synthetic-25k-5/dP/dPkP/update_stream.txt -q dataset/synthetic-25k-5/dP/dPkP/query_keywords_list-5.txt -t 0 -w 100 -k 4 -r 2 -s 3
+
+build/cdsbn -i dataset/synthetic-25k-1/dB/dBkU/initial_graph.txt -l dataset/synthetic-25k-1/dB/dBkU/label_list.txt -u dataset/synthetic-25k-1/dB/dBkU/update_stream.txt -q dataset/synthetic-25k-1/dB/dBkU/query_keywords_list-5.txt -t 0 -w 100 -k 4 -r 2 -s 3
+build/cdsbn -i dataset/synthetic-25k-2/dB/dBkU/initial_graph.txt -l dataset/synthetic-25k-2/dB/dBkU/label_list.txt -u dataset/synthetic-25k-2/dB/dBkU/update_stream.txt -q dataset/synthetic-25k-2/dB/dBkU/query_keywords_list-5.txt -t 0 -w 100 -k 4 -r 2 -s 3
+build/cdsbn -i dataset/synthetic-25k-4/dB/dBkU/initial_graph.txt -l dataset/synthetic-25k-4/dB/dBkU/label_list.txt -u dataset/synthetic-25k-4/dB/dBkU/update_stream.txt -q dataset/synthetic-25k-4/dB/dBkU/query_keywords_list-5.txt -t 0 -w 100 -k 4 -r 2 -s 3
+build/cdsbn -i dataset/synthetic-25k-5/dB/dBkU/initial_graph.txt -l dataset/synthetic-25k-5/dB/dBkU/label_list.txt -u dataset/synthetic-25k-5/dB/dBkU/update_stream.txt -q dataset/synthetic-25k-5/dB/dBkU/query_keywords_list-5.txt -t 0 -w 100 -k 4 -r 2 -s 3
+
+build/cdsbn -i dataset/synthetic-25k-1/dP/dPkU/initial_graph.txt -l dataset/synthetic-25k-1/dP/dPkU/label_list.txt -u dataset/synthetic-25k-1/dP/dPkU/update_stream.txt -q dataset/synthetic-25k-1/dP/dPkU/query_keywords_list-5.txt -t 0 -w 100 -k 4 -r 2 -s 3
+build/cdsbn -i dataset/synthetic-25k-2/dP/dPkU/initial_graph.txt -l dataset/synthetic-25k-2/dP/dPkU/label_list.txt -u dataset/synthetic-25k-2/dP/dPkU/update_stream.txt -q dataset/synthetic-25k-2/dP/dPkU/query_keywords_list-5.txt -t 0 -w 100 -k 4 -r 2 -s 3
+build/cdsbn -i dataset/synthetic-25k-4/dP/dPkU/initial_graph.txt -l dataset/synthetic-25k-4/dP/dPkU/label_list.txt -u dataset/synthetic-25k-4/dP/dPkU/update_stream.txt -q dataset/synthetic-25k-4/dP/dPkU/query_keywords_list-5.txt -t 0 -w 100 -k 4 -r 2 -s 3
+build/cdsbn -i dataset/synthetic-25k-5/dP/dPkU/initial_graph.txt -l dataset/synthetic-25k-5/dP/dPkU/label_list.txt -u dataset/synthetic-25k-5/dP/dPkU/update_stream.txt -q dataset/synthetic-25k-5/dP/dPkU/query_keywords_list-5.txt -t 0 -w 100 -k 4 -r 2 -s 3
