21fe9d2c2b5ebfa6aa933b19e05e080985b6640b
Watremons <z2331355763@sina.com>
2024-10-17 11:23:33 +0800

    (batchs): finish exp batch

--- batchs/vs_dataset.sh
+++ batchs/vs_dataset.sh
@@ -1,17 +1,14 @@
 cd /home/yons/projects/CD-SBN/
 
-build/cdsbn -i dataset/realworld/AM/initial_graph.txt -l dataset/realworld/AM/label_list.txt -u dataset/realworld/AM/update_stream.txt -q dataset/realworld/AM/query_keywords_list-5.txt -t 0 -w 100 -k 4 -r 2 -s 3
-# build/cdsbn -i dataset/realworld/AR/initial_graph.txt -l dataset/realworld/AR/label_list.txt -u dataset/realworld/AR/update_stream.txt -q dataset/realworld/AR/query_keywords_list-5.txt -t 0 -w 100 -k 4 -r 2 -s 3
-build/cdsbn -i dataset/realworld/BS/initial_graph.txt -l dataset/realworld/BS/label_list.txt -u dataset/realworld/BS/update_stream.txt -q dataset/realworld/BS/query_keywords_list-5.txt -t 0 -w 100 -k 4 -r 2 -s 3
-build/cdsbn -i dataset/realworld/CM/initial_graph.txt -l dataset/realworld/CM/label_list.txt -u dataset/realworld/CM/update_stream.txt -q dataset/realworld/CM/query_keywords_list-5.txt -t 0 -w 100 -k 4 -r 2 -s 3
-build/cdsbn -i dataset/realworld/CU/initial_graph.txt -l dataset/realworld/CU/label_list.txt -u dataset/realworld/CU/update_stream.txt -q dataset/realworld/CU/query_keywords_list-5.txt -t 0 -w 100 -k 4 -r 2 -s 3
-# build/cdsbn -i dataset/realworld/DV/initial_graph.txt -l dataset/realworld/DV/label_list.txt -u dataset/realworld/DV/update_stream.txt -q dataset/realworld/DV/query_keywords_list-5.txt -t 0 -w 100 -k 4 -r 2 -s 3
-build/cdsbn -i dataset/realworld/ML/initial_graph.txt -l dataset/realworld/ML/label_list.txt -u dataset/realworld/ML/update_stream.txt -q dataset/realworld/ML/query_keywords_list-5.txt -t 0 -w 100 -k 4 -r 2 -s 3
-build/cdsbn -i dataset/realworld/SX/initial_graph.txt -l dataset/realworld/SX/label_list.txt -u dataset/realworld/SX/update_stream.txt -q dataset/realworld/SX/query_keywords_list-5.txt -t 0 -w 100 -k 4 -r 2 -s 3
-build/cdsbn -i dataset/realworld/TA/initial_graph.txt -l dataset/realworld/TA/label_list.txt -u dataset/realworld/TA/update_stream.txt -q dataset/realworld/TA/query_keywords_list-5.txt -t 0 -w 100 -k 4 -r 2 -s 3
-build/cdsbn -i dataset/realworld/UF/initial_graph.txt -l dataset/realworld/UF/label_list.txt -u dataset/realworld/UF/update_stream.txt -q dataset/realworld/UF/query_keywords_list-5.txt -t 0 -w 100 -k 4 -r 2 -s 3
-# build/cdsbn -i dataset/realworld/VU/initial_graph.txt -l dataset/realworld/VU/label_list.txt -u dataset/realworld/VU/update_stream.txt -q dataset/realworld/VU/query_keywords_list-5.txt -t 0 -w 100 -k 4 -r 2 -s 3
-build/cdsbn -i dataset/realworld/WU/initial_graph.txt -l dataset/realworld/WU/label_list.txt -u dataset/realworld/WU/update_stream.txt -q dataset/realworld/WU/query_keywords_list-5.txt -t 0 -w 100 -k 4 -r 2 -s 3
-
-# build/cdsbn -i dataset/synthetic/dB/dBkL/initial_graph.txt -l dataset/synthetic/dB/dBkL/label_list.txt -u dataset/synthetic/dB/dBkL/update_stream.txt -q dataset/synthetic/dB/dBkL/query_keywords_list-5.txt -t 0 -w 100 -k 4 -r 2 -s 3
-# build/cdsbn -i dataset/synthetic/dP/dPkL/initial_graph.txt -l dataset/synthetic/dP/dPkL/label_list.txt -u dataset/synthetic/dP/dPkL/update_stream.txt -q dataset/synthetic/dP/dPkL/query_keywords_list-5.txt -t 0 -w 100 -k 4 -r 2 -s 3
+build/cdsbn -i dataset/realworld/AM/initial_graph.txt -l dataset/realworld/AM/label_list.txt -u dataset/realworld/AM/update_stream.txt -q dataset/realworld/AM/query_keywords_list-5.txt -t 0 -w 500 -k 4 -r 2 -s 3
+# build/cdsbn -i dataset/realworld/AR/initial_graph.txt -l dataset/realworld/AR/label_list.txt -u dataset/realworld/AR/update_stream.txt -q dataset/realworld/AR/query_keywords_list-5.txt -t 0 -w 500 -k 4 -r 2 -s 3
+build/cdsbn -i dataset/realworld/BS/initial_graph.txt -l dataset/realworld/BS/label_list.txt -u dataset/realworld/BS/update_stream.txt -q dataset/realworld/BS/query_keywords_list-5.txt -t 0 -w 500 -k 4 -r 2 -s 3
+build/cdsbn -i dataset/realworld/CM/initial_graph.txt -l dataset/realworld/CM/label_list.txt -u dataset/realworld/CM/update_stream.txt -q dataset/realworld/CM/query_keywords_list-5.txt -t 0 -w 500 -k 4 -r 2 -s 3
+build/cdsbn -i dataset/realworld/CU/initial_graph.txt -l dataset/realworld/CU/label_list.txt -u dataset/realworld/CU/update_stream.txt -q dataset/realworld/CU/query_keywords_list-5.txt -t 0 -w 500 -k 4 -r 2 -s 3
+build/cdsbn -i dataset/realworld/DV/initial_graph.txt -l dataset/realworld/DV/label_list.txt -u dataset/realworld/DV/update_stream.txt -q dataset/realworld/DV/query_keywords_list-5.txt -t 0 -w 500 -k 4 -r 2 -s 3
+build/cdsbn -i dataset/realworld/ML/initial_graph.txt -l dataset/realworld/ML/label_list.txt -u dataset/realworld/ML/update_stream.txt -q dataset/realworld/ML/query_keywords_list-5.txt -t 0 -w 500 -k 4 -r 2 -s 3
+build/cdsbn -i dataset/realworld/SX/initial_graph.txt -l dataset/realworld/SX/label_list.txt -u dataset/realworld/SX/update_stream.txt -q dataset/realworld/SX/query_keywords_list-5.txt -t 0 -w 500 -k 4 -r 2 -s 3
+build/cdsbn -i dataset/realworld/TA/initial_graph.txt -l dataset/realworld/TA/label_list.txt -u dataset/realworld/TA/update_stream.txt -q dataset/realworld/TA/query_keywords_list-5.txt -t 0 -w 500 -k 4 -r 2 -s 3
+build/cdsbn -i dataset/realworld/UF/initial_graph.txt -l dataset/realworld/UF/label_list.txt -u dataset/realworld/UF/update_stream.txt -q dataset/realworld/UF/query_keywords_list-5.txt -t 0 -w 500 -k 4 -r 2 -s 3
+# build/cdsbn -i dataset/realworld/VU/initial_graph.txt -l dataset/realworld/VU/label_list.txt -u dataset/realworld/VU/update_stream.txt -q dataset/realworld/VU/query_keywords_list-5.txt -t 0 -w 500 -k 4 -r 2 -s 3
+# build/cdsbn -i dataset/realworld/WU/initial_graph.txt -l dataset/realworld/WU/label_list.txt -u dataset/realworld/WU/update_stream.txt -q dataset/realworld/WU/query_keywords_list-5.txt -t 0 -w 500 -k 4 -r 2 -s 3
