4559b8d6d89d54d8938de3f2c51bc5b6a0da8ac9
Watremons <z2331355763@sina.com>
2024-10-29 17:07:45 +0800

    🦄 refactor(tools): refactor file dir

--- tools/result_extractor.py
+++ tools/result_extractor.py
@@ -21,8 +21,8 @@ if __name__ == "__main__":
     # "dP/dPkL"
     # "dP/dPkP"
     # "dP/dPkU"
-    dataset_type = "realworld"  # baseline realworld synthetic
-    dir_name = "SX"
+    dataset_type = "ablation"  # baseline realworld synthetic ablation
+    dir_name = "AM"
     folder_path = os.path.join(os.getcwd(), folder_path, dataset_type, dir_name)
     print("------{}------".format(folder_path))
 
@@ -30,7 +30,8 @@ if __name__ == "__main__":
 
     for dir_name in file_list:
         dir_path = os.path.join(folder_path, dir_name)
-        if dir_name.startswith("experiment-"):
+        # if dir_name.startswith("experiment-"):
+        if dir_name.startswith(('0', '1', '2', '3', '4', '5', '6', '7', '8', '9', "experiment-")):
             result_file_list = os.listdir(dir_path)
             result_list = []
             for file_name in result_file_list:
@@ -38,6 +39,8 @@ if __name__ == "__main__":
                     file_path = os.path.join(dir_path, file_name)
                     snapshot_process_time = 0
                     continuous_process_time = 0
+                    total_user_node = 0
+                    pruning_user_node = 0
                     query_k = 0
                     query_s = 0
                     with open(file_path, encoding='utf-8') as file_obj:
@@ -45,15 +48,21 @@ if __name__ == "__main__":
                     for line in lines:
                         if line.startswith("Query Support Threshold:"):
                             query_k = line.rstrip().split(' ')[3]
+                        # if line.startswith("Query Keywords File: "):
                         if line.startswith("Sliding Window Size:"):
                             query_s = line.rstrip().split(' ')[3]
                         if line.startswith("Snapshot Query Processing time:"):
                             snapshot_process_time = line.rstrip().split(' ')[4]
-                        if line.startswith("Average Query Process Time:"):
-                            continuous_process_time = line.rstrip().split(' ')[4]
-                    # refine_time = float(refine_time) * (float(sampling_ratio) if float(sampling_ratio) > 1 else 1)
-                    result_list.append([snapshot_process_time, continuous_process_time])
+                        if line.startswith("Total User Nodes:"):
+                            total_user_node = line.rstrip().split(' ')[3]
+                        if line.startswith("Pruning Vertices:"):
+                            pruning_user_node = line.rstrip().split(' ')[2]
+                        # if line.startswith("Average Query Process Time:"):
+                        #     continuous_process_time = line.rstrip().split(' ')[4]
+                    pruning_ratio = float(pruning_user_node)*100/float(total_user_node)
+                    result_list.append([snapshot_process_time, pruning_user_node, total_user_node,  pruning_ratio])
             print(dir_name)
-            # print(query_k, query_s)
+            print(query_k, query_s)
             for uni_result in result_list:
-                print(uni_result[1], sep="\t")
+                # print(uni_result[1], sep="\t")
+                print(uni_result[0], uni_result[1], sep="\t")
